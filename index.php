e5f3ba8cf08ab784aaabcb3fe7cae525eedeab20
szabdan <70110707+szabdan@users.noreply.github.com>
2020-11-02 15:02:08 +0100

    Create index.php
    php létrehozása

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Proba</title>
+</head>
+<body>
+    <h1> Proba oldal</h1>
+    <h2> második bekezdés </h2>
+    <h3> harmadik bekezdés </h3>
+    <h3> negyedik bekezdés </h3>
+    <h3> ötödik bekezdés </h3>
+    <?php
+        echo "kiíratás php-vel";
+    ?>
+</body>
+</html>
